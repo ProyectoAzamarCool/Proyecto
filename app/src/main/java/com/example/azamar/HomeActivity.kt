1bef2b48bd77e792f2f14f2c7fe51fca2f603036
castelar <joseluiscastelarfagoaga000@gmail.com>
2025-11-28 15:12:25 -0600

    moficicacion de layouts y agregado de pantallas (falta probar funcionalidad)

--- app/src/main/java/com/example/azamar/HomeActivity.kt
+++ app/src/main/java/com/example/azamar/HomeActivity.kt
@@ -3,13 +3,17 @@ package com.example.azamar
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val welcomeText = findViewById<TextView>(R.id.welcomeText)
-        welcomeText.text = "Hola, bienvenido. ¿En qué puedo ayudarte?"
+        val user = FirebaseAuth.getInstance().currentUser
+
+        val welcome = findViewById<TextView>(R.id.welcomeText)
+        welcome.text = "Hola ${user?.email}, bienvenido."
     }
 }
